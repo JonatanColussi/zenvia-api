db439e3ab99a62f865c51bbb228154cc81f07c8f
Jonatan Colussi <psffmjonatan@bancotopazio.com.br>
2020-04-27 10:36:11 -0300

    refactor: identation

--- index.d.ts
+++ index.d.ts
@@ -1,48 +1,58 @@
-declare module "zenvia-api" {
-    export function sendOne(conta: string, senha: string, body: object): Promise<ISendResponse>;
-    export function getStatus(conta: string, senha: string, id: any): Promise<ISmsStatus>;
-    export function list(conta: string, senha: string): Promise<IReceivedResponse>;
+declare module 'zenvia-api' {
+  export function sendOne(
+    conta: string,
+    senha: string,
+    body: object
+  ): Promise<ISendResponse>;
+  export function getStatus(
+    conta: string,
+    senha: string,
+    id: any
+  ): Promise<ISmsStatus>;
+  export function list(
+    conta: string,
+    senha: string
+  ): Promise<IReceivedResponse>;
 }
 
 declare interface ISendResponse {
-    "sendSmsResponse": {
-        "statusCode": string;
-        "statusDescription": string;
-        "detailCode": string
-        "detailDescription": string;
-    }
+  sendSmsResponse: {
+    statusCode: string;
+    statusDescription: string;
+    detailCode: string;
+    detailDescription: string;
+  };
 }
 
 declare interface IReceivedMessages {
-    "id": number
-    "dateReceived": any;
-    "mobile": string;
-    "body": string;
-    "shortcode": string;
-    "mobileOperatorName": string;
-    "mtId": string;
+  id: number;
+  dateReceived: any;
+  mobile: string;
+  body: string;
+  shortcode: string;
+  mobileOperatorName: string;
+  mtId: string;
 }
 
 declare interface IReceivedResponse {
-    "receivedResponse": {
-        "statusCode": string;
-        "statusDescription": string;
-        "detailCode": string;
-        "detailDescription": string;
-        "receivedMessages": IReceivedMessages[] | null;
-    }
+  receivedResponse: {
+    statusCode: string;
+    statusDescription: string;
+    detailCode: string;
+    detailDescription: string;
+    receivedMessages: IReceivedMessages[] | null;
+  };
 }
 
 declare interface ISmsStatus {
-    getSmsStatusResp:
-        {
-            id: string;
-            received: string|null;
-            shortcode: string|null;
-            mobileOperatorName: string|null;
-            statusCode: string;
-            statusDescription: string;
-            detailCode: string;
-            detailDescription: string;
-        }
+  getSmsStatusResp: {
+    id: string;
+    received: string | null;
+    shortcode: string | null;
+    mobileOperatorName: string | null;
+    statusCode: string;
+    statusDescription: string;
+    detailCode: string;
+    detailDescription: string;
+  };
 }
--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 module.exports = {
+  /**
+   * @return Promisse
+   */
+  sendOne: require('./lib/send_sms'),
 
-    /**
-     * @return Promisse
-     */
-    sendOne : require("./lib/send_sms"),
+  /**
+   * @return Promisse
+   */
+  getStatus: require('./lib/get_sms_status'),
 
-    /**
-     * @return Promisse
-     */
-    getStatus: require("./lib/get_sms_status"),
-
-    /**
-     * @return Promisse
-     */
-    list: require("./lib/list"),
+  /**
+   * @return Promisse
+   */
+  list: require('./lib/list'),
 };
